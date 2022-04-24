919d03df37c4573c014fd87923ff3bfbd99d3e99
francisco villa salmeron <frankvi@yahoo.com>
2022-04-24 15:58:52 -0600

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Tanya is the champion"
